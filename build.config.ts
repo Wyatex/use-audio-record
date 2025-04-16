60125a01c59cb6b37be5e4495e344a2fca7da3fb
wyatex <wyatex@qq.com>
2025-04-16 18:02:52 +0800

    chore: 去除不需要的配置

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,7 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['./src/index.ts', './src/vue.ts', './src/react.ts'],
   name: 'use-audio-record',
-  sourcemap: true,
-  declaration: true,
   externals: ['vue', 'react'],
   rollup: {
     emitCJS: true,
